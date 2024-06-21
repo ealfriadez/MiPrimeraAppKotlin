c19f589791ccade365aadd1f4c531335781e8462
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-06-21 12:07:16 -0500

    Avanzando en develop

--- app/src/main/java/pe/edu/unfv/miprimeraappkotlin/MainActivity.kt
+++ app/src/main/java/pe/edu/unfv/miprimeraappkotlin/MainActivity.kt
@@ -8,4 +8,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun declaracionDeVariables(){
+        var nombre = "Eleazar"
+        val apellido = "Alfriadez"
+        var precio = 31
+        var edad : Int
+        var peso : Double
+        var eSalto: Boolean
+    }
+
+    fun declaracionDeVariablesNull(){
+        var nombreNulo: String?
+        nombreNulo = null
+        var ciudad = null
+    }
+
+    fun declaracionDeArraylist(){
+        val participantes = ArrayList<String>()
+        participantes.add("Jesus")
+        participantes.add("Santiago")
+        participantes.add("Belen")
+        participantes.add("Jean")
+        participantes.add("Doris")
+    }
 }
